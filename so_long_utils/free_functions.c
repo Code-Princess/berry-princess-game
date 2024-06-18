1d87195cf5b214539494201a84d256936f18d413
Linda Lacsivy <llacsivy@1-C-9.42heilbronn.de>
2024-06-18 12:06:31 +0200

    leaks removed

--- so_long_utils/free_functions.c
+++ so_long_utils/free_functions.c
@@ -6,7 +6,7 @@
 /*   By: llacsivy <llacsivy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/16 19:54:04 by llacsivy          #+#    #+#             */
-/*   Updated: 2024/06/17 23:48:13 by llacsivy         ###   ########.fr       */
+/*   Updated: 2024/06/18 11:50:37 by llacsivy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,18 @@ void	free_princess_game(t_game *game)
 	free(game->game_window_size);
 	free_matrix_entries(game->map_data, game->matrix_size_y);
 	free(game->map_data);
-	free(game->textures[0]);
-	free(game->textures[1]);
-	free(game->textures[2]);
-	free(game->textures[3]);
-	free(game->textures[4]);
-	free(game->textures[5]);
+	mlx_delete_texture(game->textures[0]);
+	mlx_delete_texture(game->textures[1]);
+	mlx_delete_texture(game->textures[2]);
+	mlx_delete_texture(game->textures[3]);
+	mlx_delete_texture(game->textures[4]);
+	mlx_delete_texture(game->textures[5]);
 	free(game->textures);
+	mlx_delete_image(game->mlx, game->image_castle_color);
+	mlx_delete_image(game->mlx, game->image_princess);
+	mlx_delete_image(game->mlx, game->image_tree);
+	mlx_delete_image(game->mlx, game->image_strawberry);
+	mlx_delete_image(game->mlx, game->image_floor);
 }
 
 void	free_matrix_entries(char **matrix, size_t matrix_height)
